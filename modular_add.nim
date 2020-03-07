2673a043691862dd2cb1c594775260091826488d
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-03-07 15:47:19 +0100

    Lay out lazy carry according to https://github.com/mratsim/constantine/issues/15

--- modular_add.nim
+++ modular_add.nim
@@ -13,12 +13,15 @@ type
     limbs: array[wordsRequired(bits, 64), uint64]
 
   BigIntExcessBits[bits: static int] = object
-    excessLeft: uint8
+    # Note: we implement lazy carry not just lazy reduction
+    excess: uint8
     limbs: array[wordsRequired(bits, 64 - ExcessBits), uint64]
 
   Carry = cuchar
+  Borrow = cuchar
 
 # ###############################################################################
+# Constant time conditional move
 
 func mux*[T](ctl: T, x, y: T): T {.inline.}=
   ## Multiplexer / selector
@@ -54,7 +57,31 @@ func mux*[T](ctl: T, x, y: T): T {.inline.}=
       y_Mux = y
     y_Mux xor (-T(ctl) and (x_Mux xor y_Mux))
 
+func ccopy*[T](ctl: T, x: var T, y: T) {.inline.}=
+  ## Conditional copy
+  ## Copy ``y`` into ``x`` if ``ctl`` is true
+  when defined(amd64) or defined(i386):
+    when sizeof(T) == 8:
+      asm """
+        testq %[ctl], %[ctl]
+        cmovnzq %[y], %[x]
+        : [x] "+r" (`*x`)
+        : [ctl] "r" (`ctl`), [y] "r" (`y`)
+        : "cc"
+      """
+    elif sizeof(T) == 4:
+      asm """
+        testl %[ctl], %[ctl]
+        cmovnzl %[y], %[x]
+        : [x] "+r" (`*x`)
+        : [ctl] "r" (`ctl`), [y] "r" (`y`)
+        : "cc"
+      """
+  else:
+    x = mux(ctl, y, x)
+
 # ###############################################################################
+# Eager modular addition using full words
 
 func addcarry_u64(carryIn: Carry, a, b: uint64, sum: var uint64): Carry {.importc: "_addcarry_u64", header:"<x86intrin.h>", nodecl.}
 func subborrow_u64(borrowIn: Carry, a, b: uint64, diff: var uint64): Carry {.importc: "_subborrow_u64", header:"<x86intrin.h>", nodecl.}
@@ -68,7 +95,7 @@ func GT(a: BigIntCarry, b: BigIntCarry): uint64 =
   # Algo
   # a > b <=> a - b > 0, i.e. a - b does not borrow
   var diff: uint64
-  var borrow: cuchar
+  var borrow: Borrow
   for i in 0 ..< a.limbs.len:
     borrow = subborrow_u64(borrow, a.limbs[i], b.limbs[i], diff)
 
@@ -81,6 +108,8 @@ func conditionalSub(a: var BigIntCarry, b: BigIntCarry, ctl: uint64) =
   #       and mux/cmov polluting the carry flag by zero-ing it
   #       so it must be saved in register instead and added instead
   #       of an "adc"-chain with just the operands' limbs
+  #       and there is no ADOX equivalent for SBB that would only use the OF flag
+  #
   for i in 0 ..< a.limbs.len:
     borrow = subborrow_u64(borrow, a.limbs[i], b.limbs[i], diff)
     a.limbs[i] = ctl.mux(diff, a.limbs[i])
@@ -91,22 +120,57 @@ func addmod(a: var BigIntCarry, b: BigIntCarry, modulus: BigIntCarry) {.noinline
   a.conditionalSub(modulus, overflowed)
 
 # ###############################################################################
+# Lazy modular addition
+import std/bitops
 
-template isMsbSet*[T](x: T): T =
-  ## Returns the most significant bit of an integer
-  const msb_pos = T.sizeof * 8 - 1
-  x shr msb_pos
+func canonicalize(a: var BigIntExcessBits, modulus: BigIntExcessBits) =
+  ## Fully reduce a BigInt so that its representation is lesser than modulus
+  # Note: in our representation we don't shift the carries to the last word
+  #       and then use a special form like 2^255 - 19 to hand over the
+  #       extra carry to a.limbs[0].
+  #       (Lazy reduction)
+  #
+  #       Instead, we directly use limb-level multiple of the prime modulus
+  #       (Lazy carry)
+  #
+  # Assuming 8 excess bits we can hold up to 8p
+  # So conditionally substraction 4p then 2p then 1p
+  # Would always reduce to the range [0, p)
+  #
+  # Assuming we have anywhere between 5 to 7 excess bits
+  # we also want the same 4p then 2p then 1p sequence
+  #
+  # Note: if limbs[i] of the modulus is u
+  #       the maximum number of that limbs is (2^ExcessBits * u)
+  #       This is easy to deduce from addition/substraction/negation
+  #       But what about multiplication?
+  #
+  # Lazy reduction with excess bits only in the high-word instead of
+  # some excess everywhere is probably easier to use for multiplication.
+  #
+  # We would only have 2 excess bits for BN254 and 3 excess bits for BLS12-381 however.
+  static: doAssert ExcessBits > 2
+  var k = fastLog2(a.excess-1) # Only valid if we have 3+ excess bits
 
-template mask(x: uint64): uint64 =
-  x and (high(uint64) shr ExcessBits)
+  var r: BigIntExcessBits
+  while k > 0:
+    var borrow: Borrow
+    for i in 0 ..< a.limbs.len:
+      # borrow |= (a - 2^k p) < 0
+      borrow = borrow or subborrow_u64(0, a.limbs[i], modulus.limbs[i] shl k, r.limbs[i])
 
-func add_excess(a: var BigIntExcessBits, b: BigIntExcessBits) {.noinline.}=
-  ## Addition using excess bits instead of hoping for compiler carry flag
-  var excess = 0'u64
+    let ctl = uint64(borrow) xor 1 # not borrow
+    # If there was no borrow in any of the limbs previously
+    # copy r into a
+    # otherwise we substracted too much
+    # and a stays as-is
+    for i in 0 ..< a.limbs.len:
+      ctl.ccopy(a.limbs[i], r.limbs[i])
+
+func add_ignore_carry(a: var BigIntExcessBits, b: BigIntExcessBits) =
   for i in 0 ..< a.limbs.len:
-    a.limbs[i] += b.limbs[i] + excess
-    excess = a.limbs[i].isMsbSet
-    a.limbs[i] = a.limbs[i].mask()
+    a.limbs[i] += b
+  a.excess += b.excess
 
 # ###############################################################################
 import random, times, std/monotimes, strformat
